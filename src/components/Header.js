75bf42c9b7d3855b9dab31d799bf0a07e4f96cef
Gregory Bastianelli <greg@gerrg.com>
2020-08-13 14:57:14 -0400

    init commit

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="" >Logo</a>
+                    </li>
+                    <li>
+                        <a href="" >Source Code</a>
+                    </li>
+                    <li>
+                        <a target="_blank" rel="noopener noreferrer" href="https://gerrgg.github.io/resume/" >Resume</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
